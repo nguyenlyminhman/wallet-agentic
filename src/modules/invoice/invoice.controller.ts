8a9dcd5fc83f0cf032d8b41a6000ddf9b9e7b267
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-29 14:56:22 +0700

    init invoice

--- src/modules/invoice/invoice.controller.ts
+++ src/modules/invoice/invoice.controller.ts
@@ -1,12 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { EApiPath, VERSION_1 } from 'src/objects/enum/EApiPath.enum';
+import { memoryStorage } from 'multer';
+
 
 
 @ApiTags('Invoice')
 @Controller({ path: EApiPath.INVOICE, version: VERSION_1 })
 export class InvoiceController {
+    constructor() { }
 
+    @Post('/upload')
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: memoryStorage(),
+            fileFilter: (req, file, callback) => {
+                if (!file.originalname.endsWith('.pdf')) {
+                    return callback(
+                        new BadRequestException('Only pdf file allowed'),
+                        false,
+                    );
+                }
+                callback(null, true);
+            },
+        }),
+    )
+    async importRepo( @UploadedFile() file: Express.Multer.File,
+    ) {
+        if (!file) { throw new BadRequestException('File is required'); }
+        const jsonString = file.buffer.toString('utf-8');
+
+        let payload: any[];
+
+        try {
+            payload = JSON.parse(jsonString);
+        } catch (error) {
+            throw new BadRequestException('Invalid JSON file');
+        }
+
+        // const rs = await this.repoService.importRepo(payload, email);
+
+        return {result: 'rs', msg: 'Success', 'status':  HttpStatus.OK};
+    }
 
 
 }
--- src/modules/invoice/invoice.service.ts
+++ src/modules/invoice/invoice.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class InvoiceService {}
+export class InvoiceService {
+    constructor() {}
+
+    
+}
